4f498670395319b5cb2a2d15d60e56f963516abd
isadoramutarelli <isadora.mutarelli@hotmail.com>
2024-06-27 18:37:05 +0100

    First commit

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,30 @@
-// Your code here
+class Dragon{
+
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  //Metodo instancia: pode ser chamado em qualquer instancia da classe dragon
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!! `
+
+  }
+
+  //so pode ser chamado dentro da class Dragon
+  static getDragons(...dragons){ //... permite que o metodos aceite um numero VARIAVEL de argumentos
+
+// Retorna um array com os nomes dos dragões. map é utilizado para criar um novo array contendo apenas os nome
+//PARA CADA ELEMENTO ('DRAGON) NO ARRAY 'DRAGONS' A FUNÇÃO SETA RETORNA O VALOR DA PROPRIEDADE 'NAME' DESSE ELEMENTO
+    return dragons.map(dragon => dragon.name);
+  }
+
+  }
+
+
+  const puff = new Dragon("Puff", "green");
+  const toothless = new Dragon("Toothless", "black");
+  console.log(Dragon.getDragons(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,48 @@
-// Your code here
+const Dragon = require("./dragon");
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals,friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+
+    }
+
+    hasLifeGoals(){
+        for(let lifeGoal of this.lifeGoals){
+            console.log(`${this.name} like to ${lifeGoal}`);
+        }
+    }
+
+    helpsPeople(){
+        return `${this.name}  helprs their friend ${this.friend}`;
+
+    }
+
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+
+
+
+module.exports = FriendlyDragon;
+
+
+/* try{
+    module.exports = FriendlyDragon;
+} catch{
+    module.exports = null;
+} */
